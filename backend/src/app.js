b91c5448d057de6489da68c85131cb4947ed8cf4
AntonioToledoMonleon <toledo270336@gmail.com>
2024-09-25 17:12:18 +0200

    Configuracion middlewares y rutas

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,5 +8,15 @@ const app = express();
 //'process.env.PORT' sirve para desplegar una varibale de entorno con un puerto del sistema y si no hay usa el puero 4000 
 app.set('port', process.env.PORT || 4000) 
 
+//Middlewares
+app.use(cors())
+//Este middleware hara q cuando hagamos una peticion por GET o POST nos devuelva un JSON
+app.use(express.json())
+
+//Rutas
+app.get('/', (request, response)=>{
+    response.send("Bienvenido a mi api rest full");
+})
+
 //Exportamos el modulo 'app' para utilizarlo en otro lugar del proyecto
 module.exports = app;
