7d02fcf1debcaf0af1600158c41e2219bef6896a
AntonioToledoMonleon <toledo270336@gmail.com>
2024-09-25 17:53:26 +0200

    Borrar y actualizar usuario

--- backend/src/controller/usuarioController.js
+++ backend/src/controller/usuarioController.js
@@ -3,14 +3,14 @@ const usuarioController = {}
 const Usuario = require('../models/usuario')
 
 //GET
-usuarioController.getUsuario = async(request, response) =>{
+usuarioController.getUsuarios = async(request, response) =>{
     const usuarios = await usuario.find()
     response.json(usuarios)
 }
 
 //POST
 usuarioController.createUsuario = async(request, response) =>{
-    const {nombre, apellido, edad, telefono, email} = request.body;
+    const {nombre, apellido, edad, telefono, correo} = request.body;
     const newUsuario = new Usuario({
         nombre: nombre,
         apellido: apellido,
@@ -19,17 +19,30 @@ usuarioController.createUsuario = async(request, response) =>{
         correo: correo
     })
     await newUsuario.save();
-    response.json({message : '¡ Usuario creado correctamente !'})
+    response.json({message : '¡ Usuario creado con exito !'})
 }
 
 usuarioController.getUsuario = async(request, response) =>{
-    
+    const usuario = await Usuario.findById(request.params.id)
+    response.json({usuario})
 }
 
-usuarioController.getUsuario = async(request, response) =>{
-    
+usuarioController.deleteUsuario = async(request, response) =>{
+    const usuario = await Usuario.findByIdAndDelete(request.params.id)
+    response.json({message: '¡ Usuario elminado con exito !'})
 }
 
-usuarioController.getUsuario = async(request, response) =>{
-    
+usuarioController.updateUsuario = async(request, response) =>{
+    const {nombre, apellido, edad, telefono, correo} = request.body;
+    await Usuario.findByIdAndUpdate(request.params.id, {
+        nombre, 
+        apellido,
+        edad,
+        telefono,
+        correo
+    })
+    response.json({message: '¡ Usuario actualizado !'})
+
 }
+
+module.exports = usuarioController;
