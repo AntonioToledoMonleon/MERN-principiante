cc9b871b9dd81dea8114cf3248a45a88c1fb2bde
AntonioToledoMonleon <toledo270336@gmail.com>
2024-09-26 16:44:08 +0200

    Configuracion con react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom' 
+
+import Navegacion from "./components/Navegacion";
+import CrearUsuarios from "./components/CrearUsuarios";
+import ListaUsuario from "./components/ListaUsuario";
 
 function App() {
   return (
     <div className="">
-      <h2 className="text-center">Hola mundo</h2>
+      <Navegacion/>
+      <div class="container p-4">
+        <Routes>
+          <Route path="/" element={<ListaUsuario/>} />
+          <Route path="/CrearUsuario" element={<CrearUsuarios/>} />
+          <Route path="/edit/:id" element={<ListaUsuario/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
